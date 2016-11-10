38649be6c53b62a103464482cfdf1a4942baf045
toshko3331 <toshko9999@gmail.com>
2016-11-10 12:12:27 -0500

    Beginning to send historical data.

--- ll_source.js
+++ ll_source.js
@@ -1,3 +1,8 @@
+// TODO: Make the update of data independant of having a connection open.
+// A possible way to do this is through events. Have the update function simply
+// fire an event that there was an update and things can subscribe to that
+// event and handle it.
+
 var ws = require("nodejs-websocket");
 var http = require('http');
 var N = 100; // number of nodes
@@ -15,7 +20,7 @@ var db = { 	'type': 'FeatureCollection',
 			}
 		     }
 */
-var histData = {};	
+var histData = {};
 for(i = 0; i < N; i++) {
 	var temp = Math.random()*(85+20)-20;
 	var stance = 'upright';
@@ -40,49 +45,71 @@ for(i = 0; i < N; i++) {
 	histData[uid][time] = { 'temp': temp, 'stance':stance };
 }
 
+function updateData(id,now)
+{
+	var isUpdated = false;
+	var dataProp = db["features"][id].properties;
+	var coordinates = db["features"][id].geometry.coordinates;
+	if (dataProp.next_update < now) {
+		isUpdated = true;
+		dataProp.next_update += dataProp.interval_value;
+		//Update temp with in a random value.
+		dataProp.temp += 2*(Math.random()-0.5);
+		if (dataProp.stance == 'upright') {
+			if (Math.random() < 0.01) {
+				dataProp.stance = 'fallen';
+			}
+		}
+		if (dataProp.stance == 'fallen') {
+			if (Math.random() < 0.01) {
+				dataProp.stance = 'upright';
+			}
+		}
+		//Updating the historical records as well.
+		histData[id][now] = { 'temp':dataProp.temp, 'stance':dataProp.stance};
+	}
+	return isUpdated;
+	
+}
+
 function dynamicData(port) {
 	var server = ws.createServer(function (conn) {
-		console.log("New connection")
+		console.log("New connection");
 		conn.on("text", function (str) {
-			console.log("Received "+str)
-		})
+			console.log("Received "+str);
+		});
 		conn.on("close", function (code, reason) {
-			console.log("Connection closed")
-  		})
-  	// send simulated data
-		setInterval(function () {
-			now = Date.now();
-			for (i = 0; i<N; i++) {
-				dataProp = db["features"][i].properties;
-				coordinates = db["features"][i].geometry.coordinates;
-				if (dataProp.next_update < now) {
-					dataProp.next_update += dataProp.interval_value;
-					//Update temp with in a random value.
-					dataProp.temp += 2*(Math.random()-0.5);
-					if (dataProp.stance == 'upright') {
-						if (Math.random() < 0.01) {
-							dataProp.stance = 'fallen';
-						}
-					}
-					if (dataProp.stance == 'fallen') {
-						if (Math.random() < 0.01) {
-							dataProp.stance = 'upright';
-						}
-					}
-					//We only want to resend the "Feature" that is
-					//being updated.
-					histData[i][now] = { 'temp':dataProp.temp, 'stance':dataProp.stance};
-					try{
+			console.log("Connection closed");
+  		});
+		setInterval(function() {
+			for (i=0; i<N; i++) {
+				currntTime = Date.now();
+				isUpdated = updateData(i,currntTime);
+				if(isUpdated){
+					try {
 						conn.sendText(JSON.stringify(db['features'][i]) + '\r\n');
-						} catch (e) {  
-							}
+						//TODO: Send historical data.
+						} catch(e) {
+					}
 				}
 			}
-		}, 1);
+		}, 1)
 	}).listen(port);
 }
 
-function static_html()
+// On the connect of the client, sends the full historical data. Does not
+// update it, that is left to dynamicData(), therefore is called only 1 time.
+function historicalData(port)
+{
+	var server = ws.createServer(function (conn) {
+		try {
+			conn.sendText(JSON.stringify(histData));
+		} catch(e) {
+		}
+	}).listen(port);
+}
+
+function staticHtml()
 {
 	var server = http.createServer(function(request, response) {
 		// Website you wish to allow to connect
@@ -93,23 +120,8 @@ function static_html()
 		response.write(JSON.stringify(db));
 		response.end();
 	});
-
 	server.listen(8001);
 }
-
+historicalData(8003);
+staticHtml();
 dynamicData(8002);
-static_html();
-
-/*
-var express = require('express');
-var app = express();
-app.use(function (req, res, next) {
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-	next();
-});
-app.use(express.static('static'));
-app.listen(8081);
-*/
